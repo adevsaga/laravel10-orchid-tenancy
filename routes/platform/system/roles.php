e4d84f80b7c3582a66f725aee13eb46fad42e3ee
Leandro Chernij <lchernij@gmail.com>
2024-02-04 11:54:34 -0300

    Context-separated platform routes

---
+++ routes/platform/system/roles.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Orchid\Screens\Role\RoleEditScreen;
+use App\Orchid\Screens\Role\RoleListScreen;
+use Illuminate\Support\Facades\Route;
+use Tabuna\Breadcrumbs\Trail;
+
+// Platform > System > Roles > Role
+Route::screen('roles/{role}/edit', RoleEditScreen::class)
+    ->name('platform.systems.roles.edit')
+    ->breadcrumbs(fn (Trail $trail, $role) => $trail
+        ->parent('platform.systems.roles')
+        ->push($role->name, route('platform.systems.roles.edit', $role)));
+
+// Platform > System > Roles > Create
+Route::screen('roles/create', RoleEditScreen::class)
+    ->name('platform.systems.roles.create')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.systems.roles')
+        ->push(__('Create'), route('platform.systems.roles.create')));
+
+// Platform > System > Roles
+Route::screen('roles', RoleListScreen::class)
+    ->name('platform.systems.roles')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.index')
+        ->push(__('Roles'), route('platform.systems.roles')));
---
+++ routes/platform/system/user.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Orchid\Screens\User\UserEditScreen;
+use App\Orchid\Screens\User\UserListScreen;
+use Illuminate\Support\Facades\Route;
+use Tabuna\Breadcrumbs\Trail;
+
+// Platform > System > Users > User
+Route::screen('users/{user}/edit', UserEditScreen::class)
+    ->name('platform.systems.users.edit')
+    ->breadcrumbs(fn (Trail $trail, $user) => $trail
+        ->parent('platform.systems.users')
+        ->push($user->name, route('platform.systems.users.edit', $user)));
+
+// Platform > System > Users > Create
+Route::screen('users/create', UserEditScreen::class)
+    ->name('platform.systems.users.create')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.systems.users')
+        ->push(__('Create'), route('platform.systems.users.create')));
+
+// Platform > System > Users
+Route::screen('users', UserListScreen::class)
+    ->name('platform.systems.users')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.index')
+        ->push(__('Users'), route('platform.systems.users')));
